c77b0ad9b2a66d4c6163dac39e309f1348958fe9
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2016-06-08 15:56:34 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Juan Pablo Peñaloza Botero on 6/8/16.
+//  Copyright © 2016 Juan Pablo Peñaloza Botero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
